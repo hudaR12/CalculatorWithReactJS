54ef5020e4c9a607a6f18ca7f21fbabd9f802171
hudaR12 <huda.rauf@xloopdigital.com>
2023-04-16 06:16:50 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Components/Header';
+import Form from './Components/Form';
+// import GoalList from './Components/GoalList';
+import { useState } from 'react';
+import bg from './Photos/bg.PNG';
+
+function App() {
+   return (
+    <div className="container" style={{ backgroundImage: `url(${bg})` }} >
+     </div>
+    
+  );
+}
+
+export default App;
